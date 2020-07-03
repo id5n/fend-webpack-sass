e5cfd348895405917a16864445186138dcb777b8
id5n <iduality5n@gmail.com>
2020-07-03 11:18:22 -0400

    Adding new branch using the 'Webpack-Merge' Option

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+module.exports = merge(common, {
+  mode: "production",
+  devtool: "source-map",
+});
